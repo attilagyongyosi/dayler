3cd4d38ec5bbeb381f2994e79309162d47e19c62
attilagyongyosi <attilagyongyosi0@gmail.com>
2022-08-27 13:38:52 +0100

    tools: adding React, configuring Vite and TypeScript

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <h1>This is the beginning of a beautiful friendship!</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+const appElementId = 'app';
+const appElement = document.getElementById(appElementId) as HTMLElement;
+
+ReactDOM.createRoot(appElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
