81bab667bc750432e67f2368f9a94f4bae6494bb
attilagyongyosi <attilagyongyosi0@gmail.com>
2022-09-17 15:35:21 +0100

    fix: correct import statement in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import { App } from './app';
 
 const appElementId = 'app';
 const appElement = document.getElementById(appElementId) as HTMLElement;
