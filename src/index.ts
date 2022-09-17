1ed2eec33e96a96a716d8fddb66870eb9fbfe898
attilagyongyosi <attilagyongyosi0@gmail.com>
2022-09-17 16:03:49 +0100

    chore: removing unused index.ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-function init() {
-    const app = document.getElementById('app');
-    if (!app) { console.warn('Oh boy!'); return; }
-    app.innerText = 'This is a beginning of a beautiful friendship!';
-}
-
-init();
-
-export {};
